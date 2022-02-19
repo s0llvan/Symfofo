9e82d35d5b728e79dc7d9db9825e0f7649e87b5c
Sullivan Roirand <sullivanroirand@protonmail.com>
2022-02-19 16:29:57 +0100

    Add new topic page

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,20 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Topic;
+use App\Form\NewTopicType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TopicController extends AbstractController
 {
-    /**
-     * @Route("/topic/{id}", name="topic")
-     */
-    public function index(Topic $topic): Response
-    {
-        return $this->render('topic.html.twig', [
-            'topic' => $topic
-        ]);
-    }
+	/**
+	* @Route("/topic/{id}", name="topic")
+	*/
+	public function index(Topic $topic): Response
+	{
+		return $this->render('topic.html.twig', [
+			'topic' => $topic
+		]);
+	}
+	
+	/**
+	* @Route("/topic/{id}/create", name="new_topic")
+	*/
+	public function create(ManagerRegistry $managerRegistry, Request $request, Category $category): Response
+	{
+		$topic = new Topic();
+		$topic->setAuthor($this->getUser());
+		$topic->setCategory($category);
+
+		$form = $this->createForm(NewTopicType::class, $topic);
+		$form->handleRequest($request);
+		
+		if ($form->isSubmitted() && $form->isValid()) {
+			
+			$entityManager = $managerRegistry->getManager();
+			$entityManager->persist($topic);
+			$entityManager->flush();
+
+			return $this->redirectToRoute('topic', [
+				'id' => $topic->getId()
+			]);
+		}
+
+		return $this->render('new_topic.html.twig', [
+			'category' => $category,
+			'form' => $form->createView()
+		]);
+	}
 }
--- src/Entity/Topic.php
+++ src/Entity/Topic.php
@@ -56,151 +56,151 @@ class Topic
 	* @ORM\Column(type="bigint")
 	*/
 	private $views;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="topics")
-     */
-    private $author;
+	
+	/**
+	* @ORM\ManyToOne(targetEntity=User::class, inversedBy="topics")
+	*/
+	private $author;
 	
 	public function __construct()
-         	{
-         		$this->posts = new ArrayCollection();
-         		$this->views = 0;
-         	}
+	{
+		$this->posts = new ArrayCollection();
+		$this->views = 0;
+	}
 	
 	public function getId(): ?int
-         	{
-         		return $this->id;
-         	}
+	{
+		return $this->id;
+	}
 	
 	public function getCategory(): ?Category
-         	{
-         		return $this->category;
-         	}
+	{
+		return $this->category;
+	}
 	
 	public function setCategory(?Category $category): self
-         	{
-         		$this->category = $category;
-         		
-         		return $this;
-         	}
+	{
+		$this->category = $category;
+		
+		return $this;
+	}
 	
 	public function getMessage(): ?string
-         	{
-         		return $this->message;
-         	}
+	{
+		return $this->message;
+	}
 	
 	public function setMessage(string $message): self
-         	{
-         		$this->message = $message;
-         		
-         		return $this;
-         	}
+	{
+		$this->message = $message;
+		
+		return $this;
+	}
 	
 	public function getCreatedAt(): ?\DateTimeImmutable
-         	{
-         		return $this->createdAt;
-         	}
+	{
+		return $this->createdAt;
+	}
 	
 	public function setCreatedAt(\DateTimeImmutable $createdAt): self
-         	{
-         		$this->createdAt = $createdAt;
-         		
-         		return $this;
-         	}
+	{
+		$this->createdAt = $createdAt;
+		
+		return $this;
+	}
 	
 	public function getUpdatedAt(): ?\DateTimeImmutable
-         	{
-         		return $this->updatedAt;
-         	}
+	{
+		return $this->updatedAt;
+	}
 	
 	public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
-         	{
-         		$this->updatedAt = $updatedAt;
-         		
-         		return $this;
-         	}
+	{
+		$this->updatedAt = $updatedAt;
+		
+		return $this;
+	}
 	
 	public function getTitle(): ?string
-         	{
-         		return $this->title;
-         	}
+	{
+		return $this->title;
+	}
 	
 	public function setTitle(string $title): self
-         	{
-         		$this->title = $title;
-         		
-         		return $this;
-         	}
+	{
+		$this->title = $title;
+		
+		return $this;
+	}
 	
 	/**
 	* @return Collection|Post[]
 	*/
 	public function getPosts(): Collection
-         	{
-         		return $this->posts;
-         	}
+	{
+		return $this->posts;
+	}
 	
 	public function addPost(Post $post): self
-         	{
-         		if (!$this->posts->contains($post)) {
-         			$this->posts[] = $post;
-         			$post->setTopic($this);
-         		}
-         		
-         		return $this;
-         	}
+	{
+		if (!$this->posts->contains($post)) {
+			$this->posts[] = $post;
+			$post->setTopic($this);
+		}
+		
+		return $this;
+	}
 	
 	public function removePost(Post $post): self
-         	{
-         		if ($this->posts->removeElement($post)) {
-         			// set the owning side to null (unless already changed)
-         			if ($post->getTopic() === $this) {
-         				$post->setTopic(null);
-         			}
-         		}
-         		
-         		return $this;
-         	}
+	{
+		if ($this->posts->removeElement($post)) {
+			// set the owning side to null (unless already changed)
+			if ($post->getTopic() === $this) {
+				$post->setTopic(null);
+			}
+		}
+		
+		return $this;
+	}
 	
 	/**
 	* @ORM\PrePersist
 	* @ORM\PreUpdate
 	*/
 	public function updatedTimestamps(): void
-         	{
-         		$this->setUpdatedAt(new \DateTimeImmutable('now'));    
-         		if ($this->getCreatedAt() === null) {
-         			$this->setCreatedAt(new \DateTimeImmutable('now'));
-         		}
-         	}
+	{
+		$this->setUpdatedAt(new \DateTimeImmutable('now'));    
+		if ($this->getCreatedAt() === null) {
+			$this->setCreatedAt(new \DateTimeImmutable('now'));
+		}
+	}
 	
 	public function getViews(): ?string
-         	{
-         		return $this->views;
-         	}
+	{
+		return $this->views;
+	}
 	
 	public function setViews(string $views): self
-         	{
-         		$this->views = $views;
-         		
-         		return $this;
-         	}
-
-	public function getLastPost(): Post
-         	{
-         		return $this->posts->last();
-         	}
-
-    public function getAuthor(): ?User
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(?User $author): self
-    {
-        $this->author = $author;
-
-        return $this;
-    }
+	{
+		$this->views = $views;
+		
+		return $this;
+	}
+	
+	public function getLastPost(): ?Post
+	{
+		return $this->posts->count() ? $this->posts->last() : null;
+	}
+	
+	public function getAuthor(): ?User
+	{
+		return $this->author;
+	}
+	
+	public function setAuthor(?User $author): self
+	{
+		$this->author = $author;
+		
+		return $this;
+	}
 }
