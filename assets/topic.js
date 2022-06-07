713a1d03f8ba949c6c7cbd88f56cf35b46e1ab6c
Sullivan Roirand <sullivanroirand@protonmail.com>
2022-06-07 10:15:06 +0200

    TinyMCE bug fixed

--- assets/topic.js
+++ assets/topic.js
@@ -3,5 +3,10 @@ import './styles/topic.scss';
 import 'tinymce';
 
 tinymce.init({
-    selector: 'textarea#new_topic_message'
+    selector: 'textarea#new_topic_message',
+    setup: function(editor) {
+        editor.on('change', function (e) {
+            editor.save();
+        });
+    }
 });
